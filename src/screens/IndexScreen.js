722c09518686260c8504d19655cae7f5e496d4e1
Mae Kangsadan <maemeier@live.com>
2020-03-16 21:58:51 +0100

    create components

---
+++ src/screens/IndexScreen.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const IndexScreen = () => {
+  return (
+    <View>
+      <Text>Index screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {}
+});
+export default IndexScreen;
