08b0f5b3360547cf5f7fc1ce9494ac11395f7d5f
Mae Kangsadan <maemeier@live.com>
2020-03-17 03:41:01 +0100

    renderList

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -3,5 +3,16 @@ import React from "react";
 const BlogContext = React.createContext();
 
 export const BlogProvider = ({ children }) => {
-  return <BlogContext.Provider>{children}</BlogContext.Provider>;
+  const blogPosts = [
+    { title: "Post#1" },
+    { title: "Post#2" },
+    { title: "Post#3" },
+    { title: "Post#4" }
+  ];
+
+  return (
+    <BlogContext.Provider value={blogPosts}>{children}</BlogContext.Provider>
+  );
 };
+
+export default BlogContext;
